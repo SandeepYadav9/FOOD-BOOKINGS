feeab353f8861c1e2e1fb06a1762f0591718b0a3
Sandeep Yadav <sandeepuim10@gmail.com>
2022-06-04 19:20:41 +0530

    FORM-ORDER

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,16 +1,16 @@
 import { Fragment } from 'react';
-import  ReactDOM  from 'react-dom';
+import ReactDOM from 'react-dom';
 
-import './Modal.css';
+import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
-  return <div className='backdrop' onClick={props.onClose}/>;
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
 const ModalOverlay = (props) => {
   return (
-    <div className= 'modal'>
-      <div className='content'>{props.children}</div>
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
     </div>
   );
 };
@@ -20,8 +20,7 @@ const portalElement = document.getElementById('overlays');
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>,portalElement)},
-      {/* {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)} */}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
